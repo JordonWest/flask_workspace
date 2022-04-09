8edd77593319623049b6264e09b5a1dad840cbc3
Jordan West <jwest@drwholdings.com>
2022-04-09 00:20:38 -0500

    mvp reached

--- squid_inc/app.py
+++ squid_inc/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from models.models import Food
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def home_page():
-    return render_template('home.html'), 200
+    if request.method == 'POST':
+        Food.create(name=request.form['name'], rating=request.form['rating'])
+        return redirect('/')
+    else:
+        foods = Food.select()
+        return render_template('home.html', foods=foods), 200
 
-@app.route('/food_detail')
-def detail_page():
-    return render_template('food_detail.html'), 200
+@app.route('/food_detail/<id>', methods=['GET', 'POST'])
+def detail_page(id):
+    food = Food.get(id=id)
+    if request.method == 'GET':
+        return render_template('food_detail.html', food=food), 200
+    if request.method == 'POST':
+        food.name = request.form['name']
+        food.rating = request.form['rating']
+        food.save()
+        return redirect('/')
+
+@app.route('/food_detail/<id>/delete', methods=['POST'])
+def detail_delete(id):
+    food = Food.get(id=id)
+    food.delete_instance()
+    return redirect('/')
